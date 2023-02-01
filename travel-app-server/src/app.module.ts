838e13139256113da6ac296a874285dd71a6bb1a
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-02-01 15:03:57 +0530

    Sign In method

--- travel-app-server/src/app.module.ts
+++ travel-app-server/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- travel-app-server/src/auth/auth.service.ts
+++ travel-app-server/src/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+import { ConfigService } from '@nestjs/config/dist';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService, private config: ConfigService) {}
+
+  async signup(dto: AuthDto) {
+    //hash password
+    const hash = await argon.hash(dto.password);
+
+    //save user in db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+      throw error;
+    }
+  }
+}
