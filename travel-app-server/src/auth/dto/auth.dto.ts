d01e6b06e32784791090c559b1f741c84d94b1d6
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-02-01 14:22:52 +0530

    AuthDto & db schema

--- travel-app-server/src/auth/dto/auth.dto.ts
+++ travel-app-server/src/auth/dto/auth.dto.ts
@@ -1,1 +1,15 @@
-export class SignupDto {}
+import { IsEmail } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+} from 'class-validator/types/decorator/decorators';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
