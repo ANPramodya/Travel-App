8c1fb90b737c23ab2892c5b9a4bccf9b79664971
salilaherath <salilaherath@gmail.com>
2023-02-02 09:38:53 +0530

    Search screen setup and Theme file created

---
+++ TravelAppClient/src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { colors } from '../constants/theme';
+
+const SearchScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
