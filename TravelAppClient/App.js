b4a5351c415849d4d2367c54ba0cb5cf4be650f9
salilaherath <salilaherath@gmail.com>
2023-02-03 10:59:06 +0530

    Search Input and Tabs Created

--- TravelAppClient/App.js
+++ TravelAppClient/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './src/screens/SearchScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Travel App for the Mobile Application Development Module</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SearchScreen />
+    // <View style={styles.container}>
+    //   <Text>Travel App for the Mobile Application Development Module</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- TravelAppClient/src/screens/SearchScreen.js
+++ TravelAppClient/src/screens/SearchScreen.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { colors } from '../constants/theme';
+import SearchInput from '../components/Search/SearchInput';
+import Tabs from '../components/shared/Tabs';
+
+const tabs = [
+  {
+    title: 'All',
+    // content: () => <SearchMasonry key='all' list={SEARCH_ALL} />,
+    content: () => (
+      <View>
+        <Text>All</Text>
+      </View>
+    ),
+  },
+  {
+    title: 'Places',
+    // content: () => <SearchMasonry key='places' list={SEARCH_PLACES} />,
+    content: () => (
+      <View>
+        <Text>Places</Text>
+      </View>
+    ),
+  },
+  {
+    title: 'Hotels',
+    // content: () => <SearchMasonry key='hotels' list={SEARCH_HOTELS} />,
+    content: () => (
+      <View>
+        <Text>Hotels</Text>
+      </View>
+    ),
+  },
+];
 
 const SearchScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Search Screen</Text>
+    <View style={styles.container}>
+      <SearchInput />
+      <Tabs items={tabs} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.light,
+  },
+});
 
 export default SearchScreen;
