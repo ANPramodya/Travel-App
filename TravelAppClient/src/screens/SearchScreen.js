48af9e65d435c8919740e08be284ba1d3e82ad70
salilaherath <salilaherath@gmail.com>
2023-02-03 13:04:12 +0530

    Data file, Assets, Search cards done

--- TravelAppClient/src/screens/SearchScreen.js
+++ TravelAppClient/src/screens/SearchScreen.js
@@ -3,34 +3,21 @@ import { Text, View, StyleSheet } from 'react-native';
 import { colors } from '../constants/theme';
 import SearchInput from '../components/Search/SearchInput';
 import Tabs from '../components/shared/Tabs';
+import SearchMasonry from '../components/Search/SearchMasonry';
+import { SEARCH_ALL, SEARCH_HOTELS, SEARCH_PLACES } from '../data';
 
 const tabs = [
   {
     title: 'All',
-    // content: () => <SearchMasonry key='all' list={SEARCH_ALL} />,
-    content: () => (
-      <View>
-        <Text>All</Text>
-      </View>
-    ),
+    content: () => <SearchMasonry key='all' list={SEARCH_ALL} />,
   },
   {
     title: 'Places',
-    // content: () => <SearchMasonry key='places' list={SEARCH_PLACES} />,
-    content: () => (
-      <View>
-        <Text>Places</Text>
-      </View>
-    ),
+    content: () => <SearchMasonry key='places' list={SEARCH_PLACES} />,
   },
   {
     title: 'Hotels',
-    // content: () => <SearchMasonry key='hotels' list={SEARCH_HOTELS} />,
-    content: () => (
-      <View>
-        <Text>Hotels</Text>
-      </View>
-    ),
+    content: () => <SearchMasonry key='hotels' list={SEARCH_HOTELS} />,
   },
 ];
 
