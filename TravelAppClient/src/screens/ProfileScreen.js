8d517abde06829ff47675d21abf4890218e63d99
Shehani B.A.N <shehani.9716@gmail.com>
2023-02-08 22:58:08 +0530

    initiating profile screen

--- TravelAppClient/src/screens/ProfileScreen.js
+++ TravelAppClient/src/screens/ProfileScreen.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from '../constants/theme';
 
 const ProfileScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile</Text>
+    <View style={styles.Conatainer}>
+      <Text>Profile Screen</Text>
+      <Text>Profile Screen</Text>
+      <Text>Profile Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  Conatainer:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
 
 export default ProfileScreen;
