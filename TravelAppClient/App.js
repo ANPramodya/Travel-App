895042b7c0230c31ee71ddd98bb4d9c99d11b6a6
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-02-11 01:33:42 +0530

    profile to tabNav

--- TravelAppClient/App.js
+++ TravelAppClient/App.js
@@ -1,11 +1,10 @@
-
 import React from "react";
 import MainNavigator from "./src/navigations/MainNavigator";
 import ProfileScreen from "./src/screens/ProfileScreen";
 import SearchScreen from "./src/screens/SearchScreen";
 
 const App = () => {
-  return <ProfileScreen />;
+  return <MainNavigator />;
 };
 
 export default App;
